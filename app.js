86b41f6b946205ee9d930d1e7198cc1a2c0be0a0
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-02-29 19:32:39 +0800

    feat: add cors package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //routes
 const productRoutes = require('./api/routes/products');
@@ -20,18 +21,7 @@ mongoose.connect(
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-})
+app.use(cors());
 
 //avail routes
 app.use('/products', productRoutes);
