3871a8df2bba95253c5e286bf4935cc4bd37f8fc
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-03-08 16:44:34 +0800

    feat:
    create route for user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
 import User from '../models/user';
@@ -7,9 +8,29 @@ import User from '../models/user';
 const router = express.Router();
 
 /*
+    GET users
+*/
+router.get('/', (req, res, next) => {
+    User.find()
+    .select('_id email password')
+    .exec()
+    .then(users => {
+        if (users.length) {
+            res.status(200).json({
+                users: users
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
+})
+
+/*
     Register new user
 */
-
 router.post('/signup', (req, res, next) => {
     User.find({ email: req.body.email})
     .exec()
@@ -45,6 +66,50 @@ router.post('/signup', (req, res, next) => {
         })
     })
 })
+
+/*
+    user login
+*/
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email})
+    .exec()
+    .then(user => {
+        if (!user) {
+            return res.status(404).json({
+                message: 'Auth failed!'
+            });
+        }
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (err) {
+                return res.status(401).json({
+                    error: err
+                });
+            };
+            if (result) {
+                //create token for user
+                const token = jwt.sign(
+                {
+                    email: user.email,
+                    userId: user._id
+                },
+                    process.env.JWT_KEY,
+                {
+                    expiresIn: '1h'
+                });
+
+                res.status(200).json({
+                    message: 'Auth successful!',
+                    token: token
+                });
+            } else {
+                res.status(401).json({
+                    message: 'Auth failed!'
+                });
+            }
+        })
+    })
+})
+
 
 /*
     Delete user
